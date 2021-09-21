eeb876e30a5cd3ddd6ba9368ab11a99102bbd92b
nick3point5 <kenny.trinh4444@gmail.com>
2021-09-21 17:33:27 -0500

    fetch data for homepage

--- test/src/pages/Main.tsx
+++ test/src/pages/Main.tsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 
 function Main() {
+	const [ userData, setUserData ] = useState()
+
+	function fetchData() {
+		fetch(`http://localhost:8081/api/users/`, {
+			method: 'GET'
+		})
+		.then(res => {
+				return res.json()
+		})
+		.then(res => {
+			setUserData(res)
+		})
+	}
+
+	useEffect(() => {
+		fetchData()
+		return () => {
+			
+		}
+	}, [])
+
 	return (
 		<div>
 			HomePage
