11a64844c0d0b6aad4ed3beb58cb2ce84fbdc618
nick3point5 <kenny.trinh4444@gmail.com>
2021-09-21 19:13:16 -0500

    process env

--- test/src/pages/User.tsx
+++ test/src/pages/User.tsx
@@ -1,33 +1,68 @@
 import React,{useEffect, useState} from 'react'
 import { withRouter, useParams } from "react-router";
+import { Link } from 'react-router-dom';
+import userApiUrl from "../routes/userAPI";
 
 interface Params {
 	id: string;
 }
 
+const defaultUser ={
+	"id": "",
+	"first_name": "",
+	"last_name": "",
+	"email": "",
+	"favorite_color": "",
+	"number_of_pets": 0
+}
+
 function User() {
-	const [userData, setUserData] = useState()
+	const [userData, setUserData] = useState(defaultUser)
 	const params:Params = useParams()
 
 	function fetchDataUser() {
-		fetch(`http://localhost:8081/api/users/${params.id}`, {
+		fetch(`${userApiUrl}${params.id}`, {
 			method: 'GET'
 		})
 		.then(res => {
 				return res.json()
 		})
 		.then(res => {
-			setUserData(res)
+			setUserData({...userData,...res})
 		})
 	}
 	console.log(userData)
 	useEffect(() => {
 		fetchDataUser()
+		return () => {
+			
+		}
 	}, [])
 
 	return (
 		<div>
-			{/* {userData} */}
+			<div>
+				id : {userData.id}
+			</div>
+			<div>
+			email : {userData.email}
+			</div>
+			<div>
+			first name : {userData.first_name}
+			</div>
+			<div>
+			last name : {userData.last_name}
+			</div>
+			<div>
+			favorite color : {userData.favorite_color}
+			</div>
+			<div>
+			number of pets : {userData.number_of_pets}
+			</div>
+
+			<Link to={{ pathname: `/users`}} title="">
+						home
+			</Link>
 		</div>
 	)
 }
---
+++ test/src/routes/userAPI.tsx
@@ -0,0 +1,9 @@
+let userApiUrl:string
+
+if (process.env.REACT_APP_PRODUCTION) {
+  userApiUrl = `${process.env.REACT_APP_SEISMO_API_URL}`
+} else {
+  userApiUrl = 'http://127.0.0.1:8081/api/users/'
+}
+
+export default userApiUrl
