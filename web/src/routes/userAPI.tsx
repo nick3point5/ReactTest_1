642e68518487644b055b7219e771db0abd2516f6
nick3point5 <kenny.trinh4444@gmail.com>
2021-09-21 20:32:55 -0500

    changed port

--- web/src/routes/userAPI.tsx
+++ web/src/routes/userAPI.tsx
@@ -1,7 +1,7 @@
 let userApiUrl:string
 
 if (process.env.REACT_APP_PRODUCTION) {
-  userApiUrl = `${process.env.REACT_APP_SEISMO_API_URL}`
+  userApiUrl = `${process.env.REACT_APP_API_URL}`
 } else {
   userApiUrl = 'http://127.0.0.1:8081/api/users/'
 }
