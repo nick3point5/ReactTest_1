9e825f2f0b4cd0ae46b349587165836673dce854
nick3point5 <kenny.trinh4444@gmail.com>
2021-09-22 20:18:40 -0500

    slight performance boost

--- web/src/components/UserDetails/UserDetails.tsx
+++ web/src/components/UserDetails/UserDetails.tsx
@@ -4,41 +4,38 @@ import { UserData } from "../../types/types";
 import paw from "../../assets/Paw_Print.svg";
 
 function UserDetails(props: UserData) {
-  const userData = props.userData;
-  const pawElement = <img src={paw} alt="" style={{ height: 50 }} />;
-  let paws;
-  if (userData.number_of_pets < 100) {
-    paws = Array(userData.number_of_pets).fill(pawElement);
-  } else {
-    paws = Array(100).fill(pawElement);
-  }
+	const userData = props.userData;
 
-  useEffect(() => {
-    document.body.style.backgroundColor = userData.favorite_color;
-    return () => {
-      document.body.style.backgroundColor = "lightskyblue";
-    };
-  }, [userData]);
+	useEffect(() => {
+		document.body.style.backgroundColor = userData.favorite_color;
+		return () => {
+			document.body.style.backgroundColor = "lightskyblue";
+		};
+	}, [userData]);
 
-  return (
-    <div className="card">
-      <div className="card-body">
-        <h1>
-          {userData.last_name}, {userData.first_name}
-        </h1>
-        <h4>{userData.email}</h4>
+	return (
+		<div className="card">
+			<div className="card-body">
+				<h1>
+					{userData.last_name}, {userData.first_name}
+				</h1>
+				<h4>{userData.email}</h4>
 
-        <h5>Favorite Color: {userData.favorite_color}</h5>
-        <h5>
-          {userData.number_of_pets !== 1
-            ? `${userData.number_of_pets} pets`
-            : `${userData.number_of_pets} pet`}
-        </h5>
-        {paws}
-        <div>{userData.id}</div>
-      </div>
-    </div>
-  );
+				<h5>Favorite Color: {userData.favorite_color}</h5>
+				<h5>
+					{userData.number_of_pets !== 1
+						? `${userData.number_of_pets} pets`
+						: `${userData.number_of_pets} pet`}
+				</h5>
+				{userData.number_of_pets < 100
+					? Array(userData.number_of_pets).fill(
+							<img src={paw} alt="" style={{ height: 50 }} />
+					  )
+					: Array(100).fill(<img src={paw} alt="" style={{ height: 50 }} />)}
+				<div>{userData.id}</div>
+			</div>
+		</div>
+	);
 }
 
 export default UserDetails;
